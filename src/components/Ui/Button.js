bd70d6a66a215fcc2d8c219e873a7fc20f3bed8c
Lee Hour <lhour999@gmail.com>
2023-02-12 05:04:44 +0700

    styled error messages from input

--- src/components/Ui/Button.js
+++ src/components/Ui/Button.js
@@ -6,12 +6,11 @@ const Button = ({ onClick, className, type, btnName }) => {
       onClick={onClick}
       type={type}
       className={
-        "border-none outline-none bg-[#1876f2] py-[8.5px] px-[12px] rounded-[7px] font-[600] text-[14px] text-white flex items-center justify-center gap-[6px]" +
-        " " +
+        " border-none outline-none flex  items-center justify-center bg-[#1876f2] rounded-[7px]  text-white " +
         className
       }
     >
-      {btnName}
+     <p className="m-0">{btnName}</p>
     </button>
   );
 };
