d7d81a613801f8396c1169a1e2ece4946eb82af2
Lee Hour <lhour999@gmail.com>
2023-02-15 10:47:13 +0700

    done w the header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Logo, Search } from "../../svg";
+import { Logo, Search, Messenger, Notifications, ArrowDown } from "../../svg";
+import arrowDown from "../../assets/arrowDown.png";
+
 export default function Header() {
+  const { user } = useSelector((user) => ({ ...user }));
   const color = "#65676b";
   return (
     <header className="fixed top-0 h-14 z-50  bg-white w-full shadow-sm py-[5px] px-[1rem] shadow-black/10 flex items-center justify-between">
@@ -9,52 +13,38 @@ export default function Header() {
           <Logo />
         </div>
       </Link>
-      <div className="flex items-center w-[42.25rem] gap-[8px] rounded-3xl bg-[#f0f2f5] py-[10px] pr-[32px] pl-[10px] search1">
-        <Search color={color} />
+      <div className="relative flex items-center w-[42.25rem] rounded-3xl bg-[#f0f2f5]  search1">
+        <div className="absolute left-2">
+          <Search color={color} />
+        </div>
         <input
           type="text"
           placeholder="Search Facebook"
-          className="hide_input pl-1 rounded-lg outline-none border-none bg-transparent text-[15px] placeholder:translate-y-[-1px]"
+          className="py-[10px] pr-[32px] pl-[2rem] outline-none w-full rounded-3xl hover:bg-white/40 border-none bg-transparent text-[15px]"
         />
       </div>
-      <div>profile</div>
-
-      {/* <nav className="flex items-center gap-[14px] translate-x-[3px] ">
-        <li className="relative flex items-center justify-center w-[125px] h-[50px] rounded-[10px] cursor-pointer translate-x-[-2px] active:border-b-[3px]border-b-[#1876f2] ">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "border-b-[#1876f2]" : "")}
-            end
-          >
-            <HomeActive />
-          </NavLink>
-        </li>
-        <NavLink
-          to="/"
-          className="relative flex items-center justify-center w-[125px] h-[50px] rounded-[10px] cursor-pointer translate-x-[-2px]"
+      <div className="flex">
+        <div className="relative hover:bg-black/20 h-10 w-10 rounded-[50%] flex items-center justify-center mr-[8px] bg-[#e4e6eb] cursor-pointer">
+          <Messenger />
+        </div>
+        <div className="relative h-10 hover:bg-black/20 w-10 rounded-[50%] flex items-center justify-center mr-[8px] bg-[#e4e6eb] cursor-pointer">
+          <Notifications />
+          <div className="absolute -top-[0.45rem] -right-1 bg-[#FF0000] text-white text-[13px] w-[1.3rem] h-[1.3rem] rounded-full text-center  ">
+            5
+          </div>
+        </div>
+        <Link
+          to="/profile"
+          className="relative h-10 hover:bg-black/20 w-10 rounded-[50%] flex items-center justify-center  bg-[#e4e6eb] cursor-pointer "
         >
-          <Friends color={color} />
-        </NavLink>
-        <NavLink
-          to="/"
-          className="relative flex items-center justify-center w-[125px] h-[50px] rounded-[10px] cursor-pointer translate-x-[-2px] hover1"
-        >
-          <Watch color={color} />
-          <div className="middle_notification">9+</div>
-        </NavLink>
-        <NavLink
-          to="/"
-          className="relative flex items-center justify-center w-[125px] h-[50px] rounded-[10px] cursor-pointer translate-x-[-2px] hover1"
-        >
-          <Market color={color} />
-        </NavLink>
-        <NavLink
-          to="/"
-          className="relative flex items-center justify-center w-[125px] h-[50px] rounded-[10px] cursor-pointer translate-x-[-2px] hover1"
-        >
-          <Gaming color={color} />
-        </NavLink>
-      </nav> */}
+          <img
+            className="w-full h-full rounded-full"
+            src={user?.picture}
+            alt=""
+          />
+          <img className="absolute top-[1.6rem] right-0 outline-2 outline-double outline-black/20 w-[0.8rem] h-[0.8rem]  rounded-full bg-white/20 " src={arrowDown} alt="" />
+        </Link>
+      </div>
     </header>
   );
 }
