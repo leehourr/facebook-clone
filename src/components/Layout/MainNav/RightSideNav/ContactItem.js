ce44b8c001edc911b225616be7a8bc12bd4cb344
Lee Hour <lhour999@gmail.com>
2023-02-26 15:09:14 +0700

    ...

--- src/components/Layout/MainNav/RightSideNav/ContactItem.js
+++ src/components/Layout/MainNav/RightSideNav/ContactItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContactItem = () => {
   return (
-    <div className="cursor-pointer rounded-lg p-2 hover:bg-black/5 w-full flex items-center">
+    <div className="cursor-pointer select-none rounded-lg p-2 hover:bg-black/5 w-full flex items-center">
       <img className="w-7 h-7 rounded-full bg-black/10" alt="" />
       <span className="ml-3">Name</span>
     </div>
